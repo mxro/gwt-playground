de10644089e4ac47f0041225b23f43f205f62ed4
mxro <mxro@nowhere.com>
2015-12-03 07:47:34 +1100

    Reworking JS exceptions Autocommit Thu Dec 03 07:47:34 EST 2015

--- src/main/java/de/mxro/gwtplayground/internal/PlaygroundEntryPoint.java
+++ src/main/java/de/mxro/gwtplayground/internal/PlaygroundEntryPoint.java
@@ -2,8 +2,7 @@ package de.mxro.gwtplayground.internal;
 
 import delight.gwt.console.Console;
 
-import java.io.PrintWriter;
-import java.io.StringWriter;
+import java.util.Arrays;
 
 import org.timepedia.exporter.client.ExporterUtil;
 
@@ -29,10 +28,7 @@ public class PlaygroundEntryPoint {
             @Override
             public void onUncaughtException(final Throwable e) {
 
-                final StringWriter sw = new StringWriter();
-                final PrintWriter pw = new PrintWriter(sw);
-                e.printStackTrace(pw);
-                Console.log(sw.toString());
+                Console.log(Arrays.toString(e.getStackTrace()));
                 // Console.log(e.toString());
                 throw new RuntimeException(e);
             }
