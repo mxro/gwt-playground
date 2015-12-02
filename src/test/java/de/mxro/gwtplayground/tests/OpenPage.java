1d1abedc1c3a9342159be99ed4829cd26eca97e4
mxro <mxro@nowhere.com>
2015-12-02 18:07:58 +1100

    Reworking JS exceptions Autocommit Wed Dec 02 18:07:58 EST 2015

--- src/test/java/de/mxro/gwtplayground/tests/OpenPage.java
+++ src/test/java/de/mxro/gwtplayground/tests/OpenPage.java
@@ -7,7 +7,7 @@ public class OpenPage {
     public static void main(final String[] args) {
 
         Spawn.sh("/opt/google/chrome/google-chrome --allow-file-access-from-files "
-                + "file:///data/databases/git/appjangle-client/appjangle-javascript-local/src/main/webapp/page.html");
+                + "file:///data/databases/git/div/gwt-playground/src/main/webapp/page.html");
 
     }
 
