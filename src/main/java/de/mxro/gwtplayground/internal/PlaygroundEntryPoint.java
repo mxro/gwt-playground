199042da9e4c7c6268d24ae41e2855f1d2e1bbed
mxro <mxro@nowhere.com>
2017-04-24 09:34:13 +1000

    Improving Stellar DB Autocommit Mon Apr 24 09:34:13 AEST 2017

--- src/main/java/de/mxro/gwtplayground/internal/PlaygroundEntryPoint.java
+++ src/main/java/de/mxro/gwtplayground/internal/PlaygroundEntryPoint.java
@@ -1,49 +1,28 @@
 package de.mxro.gwtplayground.internal;
 
-import org.timepedia.exporter.client.ExporterUtil;
-
-import com.google.gwt.core.client.GWT;
-
 public class PlaygroundEntryPoint {
 
-    private final native void onLoad()/*-{
-                                      //$wnd.onLoad();
-                                      setTimeout(function() { $wnd.onLoad(); }, 1);
-                                      }-*/;
+    private final native Object createError()/*-{
+                                             return new Error();
+                                             }-*/;
+
+    private final native boolean isError(Object obj)/*-{
+                                                    return obj instanceof Error;
+                                                    }-*/;
+
+    private final static native void log(String message)/*-{ 
+                                                        $wnd.console.log(message);
+                                                        
+                                                        }-*/;
 
     public void onModuleLoad() {
-        // GWT.create(ExportTest.class);
-        ExporterUtil.exportAll();
-        // onLoad();
 
-        GWT.setUncaughtExceptionHandler(null);
+        final Object error = createError();
 
-        /*
-         * GWT.setUncaughtExceptionHandler(new UncaughtExceptionHandler() {
-         * 
-         * @Override public void onUncaughtException(final Throwable e) {
-         * 
-         * Console.log(Arrays.toString(e.getStackTrace())); //
-         * Console.log(e.toString()); // throw new RuntimeException(
-         * "Background excpetion", e); throwError(e.getMessage()); }
-         * 
-         * });
-         */
-
-        try {
-            throwError("Hello!");
-        } catch (final Throwable t) {
-            throw new RuntimeException(t);
-            // Console.log(t.getMessage());
+        if (isError(error)) {
+            log("Was an error!");
         }
 
     }
-
-    private final static native void throwError(String message)/*-{ 
-                                                               // $wnd.console.log(t);
-                                                               var e = new Error(message);
-                                                               
-                                                               throw e;
-                                                               }-*/;
 
 }
